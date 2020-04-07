c124c7f49413ec09091ef1a19b920a489f9f7668
Omar Castorena <omarcastorena11@gmail.com>
2020-04-07 09:52:04 -0700

    updated offense.py

--- stats/offense.py
+++ stats/offense.py
@@ -1,2 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+# select all plays
+plays = games[games['type'] == 'play']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+
+# select only hits
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
+
+# convert to numeric
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
+
+# replace dictionary
+replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
+
+#r eplace function
+hit_type = hits['event'].replace(replacements, regex=True)
+
+# add a new column
+hits = hits.assign(hit_type=hit_type)
+
+# group by inning and hit type
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+
+# convert hit type to categorical
+hits['hit_type'] = pd.Categorical(['single', 'double', 'triple', 'hr'])
+
+# sort values
+hits = hits.sort_values(by=['inning', 'hit_type'])
+
+# reshape with pivot
+hits = hits.pivot(index='inning', columns='hit_type', values='count')
+
+# stacked bar plot
+hits.plot.bar(stacked=True)
+plt.show()
