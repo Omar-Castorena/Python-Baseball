1ac6e4c21a7d703f683f6fee819e578fc2068571
Omar Castorena <omarcastorena11@gmail.com>
2020-04-07 11:20:05 -0700

    updated defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -1,2 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from frames import games, info, events
+
+# query function
+plays = games.query('type == "play" and event != "NP"')
+
+# column labels
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+
+# shift dataframe
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
+
+# group plate appearances
+pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name="PA")
+
+# set the index
+events = events.set_index(['year', 'game_id', 'team', 'event_type'])
+
+# unstack the dataframe
+events = events.unstack().fillna(0).reset_index()
+
+# manage column labels
+events.columns = events.columns.droplevel()
+events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
+events = events.rename_axis(None, axis='columns')
+
+# merge - plate appearances
+events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
+
+# merge - team
+defense = pd.merge(events_plus_pa, info)
+
+#calculate DER
+defense.loc[:, 'DER'] = 1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - defense['BB'] - defense['SO'] - defense['HBP'] - defense['HR']))
+defense.loc[:, 'year'] = pd.to_numeric(defense.loc[:, 'year'])
+
+# reshape with pivot
+der = defense.loc[defense['year'] >= 1978, ['year', 'defense', 'DER']]
+der = der.pivot(index='year', columns='defense', values='DER')
+
+# plot formatting - xticks
+der.plot(x_compat=True, xticks=range(1978, 2018, 4), rot=45)
+plt.show()
+
+print(der)
